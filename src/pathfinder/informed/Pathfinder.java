051384247a95e5d1d3a89fa5713091bab43b93ce
Kevin <35283725+kpeter51@users.noreply.github.com>
2019-01-26 18:30:59 -0800

    initial hw1 commit

---
+++ src/pathfinder/informed/Pathfinder.java
@@ -0,0 +1,82 @@
+// Kevin Peters
+package pathfinder.informed;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Map;
+
+/**
+ * Maze Pathfinding algorithm that implements a basic, uninformed, breadth-first tree search.
+ */
+public class Pathfinder {
+
+    /**
+     * Given a MazeProblem, which specifies the actions and transitions available in the
+     * search, returns a solution to the problem as a sequence of actions that leads from
+     * the initial to a goal state.
+     *
+     * @param problem A MazeProblem that specifies the maze, actions, transitions.
+     * @return An ArrayList of Strings representing actions that lead from the initial to
+     * the goal state, of the format: ["R", "R", "L", ...]
+     */
+    public static ArrayList<String> solve(MazeProblem problem) {
+
+        ArrayDeque<SearchTreeNode> frontier = new ArrayDeque<>();
+
+        frontier.add(new SearchTreeNode(problem.INITIAL_STATE, null, null));
+
+        while (!frontier.isEmpty()) {
+            SearchTreeNode currentNode = frontier.poll();
+
+            if (problem.isGoal(currentNode.state)) { return generatePath(currentNode); }
+
+            Map<String, MazeState> transitions = problem.getTransitions(currentNode.state);
+            for (String t : transitions.keySet()) {
+                frontier.add(new SearchTreeNode(transitions.get(t), t, currentNode));
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * @param currentNode the final node in a path
+     * @return A ArrayList<String> of in-order steps to traverse from the problem's START_STATE to its GOAL_STATE
+     * Ex: ["R","D","D","L","U"]
+     */
+    private static ArrayList<String> generatePath(SearchTreeNode currentNode) {
+        ArrayList<String> path = new ArrayList<>();
+
+        while (currentNode.parent != null) {
+            path.add(currentNode.action);
+            currentNode = currentNode.parent;
+        }
+        Collections.reverse(path);
+        return path;
+    }
+}
+
+/**
+ * SearchTreeNode that is used in the Search algorithm to construct the Search
+ * tree.
+ */
+class SearchTreeNode {
+
+    MazeState state;
+    String action;
+    SearchTreeNode parent;
+
+    /**
+     * Constructs a new SearchTreeNode to be used in the Search Tree.
+     *
+     * @param state  The MazeState (col, row) that this node represents.
+     * @param action The action that *led to* this state / node.
+     * @param parent Reference to parent SearchTreeNode in the Search Tree.
+     */
+    SearchTreeNode(MazeState state, String action, SearchTreeNode parent) {
+        this.state = state;
+        this.action = action;
+        this.parent = parent;
+    }
+}
---
+++ src/pathfinder/informed/PathfinderTests.java
@@ -0,0 +1,83 @@
+package pathfinder.informed;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+/**
+ * Unit tests for Maze Pathfinder. Tests include completeness and
+ * optimality.
+ */
+public class PathfinderTests {
+
+    @Test
+    public void testPathfinder_t0() {
+        String[] maze = {
+                "XXXXXXX",
+                "XI...KX",
+                "X.....X",
+                "X.X.XGX",
+                "XXXXXXX"
+        };
+        MazeProblem prob = new MazeProblem(maze);
+        ArrayList<String> solution = Pathfinder.solve(prob);
+
+        // result will be a 2-tuple (isSolution, cost) where
+        // - isSolution = 0 if it is not, 1 if it is
+        // - cost = numerical cost of proposed solution
+        int[] result = prob.testSolution(solution);
+        assertEquals(1, result[0]); // Test that result is a solution
+        assertEquals(6, result[1]); // Ensure that the solution is optimal
+    }
+
+    @Test
+    public void testPathfinder_t1() {
+        String[] maze = {
+                "XXXXXXX",
+                "XI....X",
+                "X.MMM.X",
+                "X.XKXGX",
+                "XXXXXXX"
+        };
+        MazeProblem prob = new MazeProblem(maze);
+        ArrayList<String> solution = Pathfinder.solve(prob);
+
+        int[] result = prob.testSolution(solution);
+        assertEquals(1, result[0]);  // Test that result is a solution
+        assertEquals(14, result[1]); // Ensure that the solution is optimal
+    }
+
+    @Test
+    public void testPathfinder_t2() {
+        String[] maze = {
+                "XXXXXXX",
+                "XI.G..X",
+                "X.MMMGX",
+                "X.XKX.X",
+                "XXXXXXX"
+        };
+        MazeProblem prob = new MazeProblem(maze);
+        ArrayList<String> solution = Pathfinder.solve(prob);
+
+        int[] result = prob.testSolution(solution);
+        assertEquals(1, result[0]);  // Test that result is a solution
+        assertEquals(10, result[1]); // Ensure that the solution is optimal
+    }
+
+    @Test
+    public void testPathfinder_t3() {
+        String[] maze = {
+                "XXXXXXX",
+                "XI.G..X",
+                "X.MXMGX",
+                "X.XKX.X",
+                "XXXXXXX"
+        };
+        MazeProblem prob = new MazeProblem(maze);
+        ArrayList<String> solution = Pathfinder.solve(prob);
+
+        assertNull(solution); // Ensure that Pathfinder knows when there's no solution
+    }
+}
