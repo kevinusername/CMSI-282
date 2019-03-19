9ab2b4c3267534376f16197ac64356ca67bfc504
Kevin <35283725+kevinusername@users.noreply.github.com>
2019-03-19 01:10:53 -0700

    early stages of bottom-up

--- src/lcs/LCS.java
+++ src/lcs/LCS.java
@@ -1,61 +1,129 @@
 package lcs;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class LCS {
-    
+
     /**
      * memoCheck is used to verify the state of your tabulation after
      * performing bottom-up and top-down DP. Make sure to set it after
      * calling either one of topDownLCS or bottomUpLCS to pass the tests!
      */
     public static int[][] memoCheck;
-    
+
     // -----------------------------------------------
     // Shared Helper Methods
     // -----------------------------------------------
-    
+
     // [!] TODO: Add your shared helper methods here!
-    
+
+    private static Set<String> genString(int[][] solTable, String rStr, String cStr, int row, int col, StringBuilder curSol) {
+        HashSet<String> allSolutions = new HashSet<>();
+        int currentVal = solTable[row][col];
+
+
+        while (currentVal > 0) {
+            if (rStr.charAt(row - 1) == cStr.charAt(col - 1)) {
+                curSol.append(rStr.charAt(row - 1));
+                row--;
+                col--;
+            } else {
+                if (solTable[row - 1][col] > solTable[row][col - 1]) {
+                    if (solTable[row - 1][col] > currentVal) {
+                        curSol.append(rStr.charAt(row - 2));
+                    }
+                    row--;
+                } else if (solTable[row - 1][col] < solTable[row][col - 1]) {
+                    if (solTable[row][col - 1] > currentVal) {
+                        curSol.append(cStr.charAt(col - 2));
+                    }
+                    col--;
+                } else {
+                    allSolutions.addAll(genString(solTable, rStr, cStr, row - 1, col, curSol));
+                    allSolutions.addAll(genString(solTable, rStr, cStr, row, col - 1, curSol));
+                    break;
+                }
+            }
+            currentVal = solTable[row][col];
+        }
+        if (currentVal == 0) {
+            allSolutions.add(curSol.toString());
+        }
+
+        return allSolutions;
+    }
+
 
     // -----------------------------------------------
     // Bottom-Up LCS
     // -----------------------------------------------
-    
+
     /**
      * Bottom-up dynamic programming approach to the LCS problem, which
      * solves larger and larger subproblems iterative using a tabular
      * memoization structure.
+     *
      * @param rStr The String found along the table's rows
      * @param cStr The String found along the table's cols
      * @return The longest common subsequence between rStr and cStr +
-     *         [Side Effect] sets memoCheck to refer to table
+     * [Side Effect] sets memoCheck to refer to table
      */
-    public static Set<String> bottomUpLCS (String rStr, String cStr) {
+    public static Set<String> bottomUpLCS(String rStr, String cStr) {
+        int[][] table = new int[rStr.length() + 1][cStr.length() + 1];
+        int row = 1, col = 1;
+
+        while (true) {
+            if (col > cStr.length() + 1) break;
+            for (int curCol = col; curCol < cStr.length() + 1; curCol++) {
+                fillCell(rStr, cStr, table, row, curCol);
+            }
+            row++;
+
+            if (row > rStr.length() + 1) break;
+            for (int curRow = row; curRow < rStr.length() + 1; curRow++) {
+                fillCell(rStr, cStr, table, curRow, col);
+            }
+            col++;
+        }
+
+        Set<String> solution = genString(table, rStr, cStr, rStr.length(), cStr.length(), new StringBuilder());
+
+
         throw new UnsupportedOperationException();
+
     }
-    
+
+    private static void fillCell(String rStr, String cStr, int[][] table, int curRow, int curCol) {
+        if (rStr.charAt(curRow - 1) == cStr.charAt(curCol - 1)) {
+            table[curRow][curCol] = table[curRow - 1][curCol - 1] + 1;
+        } else {
+            table[curRow][curCol] = Math.max(table[curRow - 1][curCol], table[curRow][curCol - 1]);
+        }
+    }
+
     // [!] TODO: Add any bottom-up specific helpers here!
-    
-    
+
+
     // -----------------------------------------------
     // Top-Down LCS
     // -----------------------------------------------
-    
+
     /**
      * Top-down dynamic programming approach to the LCS problem, which
      * solves smaller and smaller subproblems recursively using a tabular
      * memoization structure.
+     *
      * @param rStr The String found along the table's rows
      * @param cStr The String found along the table's cols
      * @return The longest common subsequence between rStr and cStr +
-     *         [Side Effect] sets memoCheck to refer to table  
+     * [Side Effect] sets memoCheck to refer to table
      */
-    public static Set<String> topDownLCS (String rStr, String cStr) {
+    public static Set<String> topDownLCS(String rStr, String cStr) {
         throw new UnsupportedOperationException();
     }
-    
+
     // [!] TODO: Add any top-down specific helpers here!
-    
-    
+
+
 }
